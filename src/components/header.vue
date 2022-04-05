b2892e5843b77ed3dc4a9c2b94a0ea1d2458023c
Sutharshan <sutharshankanna04@gmail.com>
2022-04-05 21:28:03 +0530

    Signup navigation

--- src/components/header.vue
+++ src/components/header.vue
@@ -25,6 +25,9 @@
                       <li class="nav-item">
                         <a class="page-scroll" href="#">Login</a>
                       </li>
+                      <li class="nav-item">
+                        <a class="page-scroll" href="#">Signup</a>
+                      </li>
                     </ul>
                   </div>
                 </nav>
