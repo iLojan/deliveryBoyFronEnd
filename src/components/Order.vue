21b35d678f775f5e7a2b26cb6729366d5f2ab71e
ilojan <i.lojan@yahoo.com>
2022-04-12 08:46:02 +0530

    added select vehicle type popup

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="md:px-8  lg:w-10/12 md:10/12 w-full md:m-auto  md:p-0 p-3 md:mt-20">
     <div class="main">
+      
       <div class="grid grid-cols-12 gap-4 px-3">
         <div class="col-span-4 left-grid scrollbar pr-3" id="style-2">
           <div class="searchCity">
@@ -18,6 +19,9 @@
               @click="pickUpLocation()">PickUp Location</button>
           </div>
           <div class="">
+            <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="modal" data-bs-target="#exampleModalLg">Large modal</button>
+            <button class="mb-3 border-2 py-2 w-full px-3 rounded border- font-medium "
+            data-bs-toggle="modal" data-bs-target="#exampleModalLg">Select Vehile type</button>
             <div class="">
               <label>Select Vehile type </label>
               <select name="" class="input border py-2 w-full mb-3" id="">
@@ -56,12 +60,29 @@
           </div>
         </div>
         <div class="col-span-8">
-          <Map :disableUI="false" :zoom="12" mapType="hybrid" :center="{ lat: 38.8977859, lng: -77.0057621 }"
+          <Map :disableUI="false" :zoom="12" mapType="hybrid" @distance="distance" :center="{ lat: 38.8977859, lng: -77.0057621 }"
             :originLat="originLat" :originLng="originLng" :destinationLat="destinationLat"
             :destinationLng="destinationLng"></Map>
         </div>
       </div>
 
+      <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="exampleModalLg" tabindex="-1" aria-labelledby="exampleModalLgLabel" aria-modal="true" role="dialog">
+        <div class="modal-dialog modal-lg relative w-auto pointer-events-none">
+          <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+            <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+              <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalLgLabel">
+                Large modal
+              </h5>
+              <button type="button"
+                class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body relative p-4">
+              <SelectVehehileTypeModel/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -70,14 +91,17 @@
 <script>
   import Map from "./Map.vue"
   import axios from 'axios'
+  import SelectVehehileTypeModel from './SelectVehehileTypeModel.vue'
   export default {
-    components: { Map },
+    components: { Map,SelectVehehileTypeModel },
     data() {
       return {
         originLat: 8.5832926,
         originLng: 81.1781113,
         destinationLat: '',
         destinationLng: '',
+        showSelectVehehileType:false,
+        setDistance:'',
         latLng: {
           originLatitude: null,
           originLongitude: null,
@@ -94,11 +118,17 @@
     },
     computed: {
       gerMergeArray() {
-
         return this.$store.getters.getLatLng
       }
     },
     methods: {
+      selectVehehileType(){
+        this.showSelectVehehileType = true;
+      },
+      distance(event){
+        this.setDistance = event;
+      console.log("getDistance",event);
+      },
       test() {
         this.$store.dispatch("setDynamicLoop", this.testPosition);
       },
